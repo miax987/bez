5daaba5df5b547c43d2c3b722e1bcd45d7b1f180
hai dong <hai_dong1@yahoo.com>
2022-05-01 00:16:39 +0200

    use mariadb

--- hubbackend/app/models/index.js
+++ hubbackend/app/models/index.js
@@ -2,17 +2,10 @@ const dbConfig = require("../config/db.config.js");
 
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-  port: dbConfig.port,
+  host: "127.0.0.1",
+  dialect: "mariadb",
+  port: "3306",
   operatorsAliases: false,
-
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle
-  }
 });
 
 const db = {};
